371e209e9c1dd9073b39d4b3118d6cf234126538
unknown <skynet0989@gmail.com>
2026-05-21 09:20:04 +0700

    phan4

--- KS25_CNTT6_DoQuangVi_007.sql
+++ KS25_CNTT6_DoQuangVi_007.sql
@@ -30,7 +30,7 @@ CREATE TABLE appointments(
     doctor_id INT,
     appointment_time DATETIME NOT NULL,
     fee DECIMAL(10,2) CHECK(fee > 0),
-    appointment_status VARCHAR(9) CHECK(  appointment_status IN ('Booked','Complete','Cancelled')) ,
+    appointment_status VARCHAR(9) CHECK(  appointment_status IN ('Booked','Completed','Cancelled')) ,
 	FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
     FOREIGN KEY (doctor_id)  REFERENCES doctors(doctor_id)
 );
@@ -79,9 +79,9 @@ VALUES('BS. Nguyen Van Hai','Noi','0931112223', 4.8 ),
 
 INSERT INTO appointments
 VALUES('7001',1,1,'2024-05-20 8:00',200000,'Booked'),
-	('7002',2,2,'2024-05-20 9:30',250000,'Complete'),
+	('7002',2,2,'2024-05-20 9:30',250000,'Completed'),
     ('7003',3,3,'2024-05-20 10:15', 300000,'Booked'),
-    ('7004',4,5,'2024-05-21 7:00',350000,'Complete'),
+    ('7004',4,5,'2024-05-21 7:00',350000,'Completed'),
     ('7005',5,4,'2024-05-21 8:45',220000,'Cancelled');
     
     -- insert medical_records
@@ -107,5 +107,44 @@ VALUES('8003',1,'2024-05-20 9:05','Đã khám lần đầu'),
     -- Câu 2 phần 2:
     -- Tăng 10% phí khám cho phiếu hẹn có trạng thái commplete và bệnh nhân sinh < 2000
     -- xóa bản ghi trong visit log thỏa mãn logtime trước 20/05/2024
+	SET SQL_SAFE_UPDATES = 0;
+     
+    UPDATE appointments AS a -- câu 1
+    JOIN patients AS p
+    ON a.patient_id = p.patient_id
+    SET a.fee = a.fee * 1.1 
+    WHERE a.appointment_status ='Completed' AND YEAR(p.date_of_birth) < 2000;
     
-    UPDATE 
+    DELETE FROM visit_log -- câu 2
+    WHERE log_time < '2024-05-20'; -- không mất dữ liệu vì không có ai khám trước ngày 20
+    
+    -- Phần 3
+    -- câu 1: liệt kê thông tin bác sĩ: full_name, specialty và rating của những bác sĩ có rating > 4.7 hoặc khoa Nhi
+    -- câu 2: liệt kê thông tin bệnh nhân: full_name, phone_number của những bệnh nhân có ngày sinh BETWEEN '1998-01-01' AND '2001-31-12' và sdt LIKE'090%'
+    -- câu 3 lieket kê các phiếu hẹn: appointment_id, appointment_time, fee được sắp xếp desc và limit 2 , offset 2
+    
+	SELECT full_name,specialty,rating FROM doctors -- câu 1
+    WHERE rating > 4.7 OR specialty = 'Nhi';
+    
+    SELECT full_name,phone_number FROM patients -- câu 2
+    WHERE phone_number LIKE'090%' AND date_of_birth BETWEEN '1998-01-01' AND '2001-31-12';
+    
+    SELECT appointment_id, appointment_time, fee -- câu 3
+    FROM appointments
+    ORDER BY fee DESC
+    LIMIT 2 OFFSET 2;
+    
+    
+    -- phần 4
+    -- liệt kê thông tin: full_name của patient và doctor , chuyên khoa, phí khám,và thời điểm hẹn khám 
+    SELECT p.full_name AS 'Tên bệnh nhân',d.full_name AS 'Tên bác sĩ',d.specialty,a.fee,a.appointment_time
+    FROM appointments AS a
+    JOIN doctors AS d
+    ON a.doctor_id = d.doctor_id
+    JOIN patients AS p
+    ON a.patient_id = p.patient_id;
+    
+    -- liệt kê thông tin bác sĩ gồm full_name và tổng phí khám bác sĩ đó đã thực hiện , chỉ tính phiếu completed, chỉ hiển thị bác sĩ có tổng chi phí lớn hơn 500000
+    
+    
+    
